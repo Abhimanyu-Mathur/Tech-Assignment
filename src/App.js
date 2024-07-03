3a31d6df06d09c39a5b9ac2f0cba8371cfbebcb5
Abhimanyu-Mathur <mabhimanyu05@gmail.com>
2024-07-03 21:35:35 +0100

    Add App component and initial files in src folder
    Added App component and necessary files present inside src folder. Excluded components inside components folder from this commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GlobalSearch from './components/GlobalSearch';
+
+
+function App() {
+  return (
+    <>
+      <GlobalSearch />
+    </>
+  );
+}
+
+export default App;
